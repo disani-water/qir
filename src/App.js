c86ea0971c41ba9feb60bf14c0d50b3e825b663e
disani-water <s1821740@online.houstonisd.org>
2023-04-14 02:01:26 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
+const tree = {
+title: 'trees',
+imgUrl: 'https://media.istockphoto.com/id/155097073/photo/old-beech-tree-in-summer.jpg?s=612x612&w=0&k=20&c=cTuBEG1nIVC9FAlwSpIAbUxUc_QSUgUzjWDFzYA_8ek=',
+
+};
+
+
 function App() {
   return (
-    <div>
-      <h1> hello SHEERR RAAYYY</h1>
-    </div>
-  );
-}
+ <><h1 className='c'>{tree.title}</h1>
+ <p className='c'> I like trees so I make website to tell about trees</p>
+ <img src={tree.imgUrl}/>
+ </>
+  )
+};
 
 export default App;
