a9965bfd1e3e3e9c9da822842e26d9a3af8d0080
disani-water <s1821740@online.houstonisd.org>
2023-05-29 20:44:10 -0500

    topbutton

--- src/navbar.js
+++ src/navbar.js
@@ -6,10 +6,10 @@
  
  export default function Nav () {
     return(
-//the 
+
 <nav>
     <Stack direction="horizontal" gap={4} >
-         <a href="#qa"><h4 className='underline'> Quaking Aspen</h4> </a>
+        <a href="#qa"><h4 className='underline'> Quaking Aspen</h4> </a>
         <a href="#bs"> <h4 className="underline"> Bald Cypress</h4> </a>
         <a href="#pp"> <h4 className="underline"> Ponderosa Pine</h4> </a>
     </Stack>
